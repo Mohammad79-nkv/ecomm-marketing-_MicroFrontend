4c977923ca9d04007fbf548a81d6065e35d61d97
mohammad <mohammadnv79@gmail.com>
2023-08-15 16:08:51 +0330

    init project

---
+++ config/webpack.common.js
@@ -0,0 +1,24 @@
+module.export = {
+  mode: "development",
+  devServer: {
+    port: 8081,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            Plugin: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+};
